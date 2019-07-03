f2af907afa1633c27b5f314741c9a128857e3508
Gowsalya53 <52441994+Gowsalya53@users.noreply.github.com>
2019-07-03 16:55:44 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/Features/Register.feature","src/test/resources/Features/Login2.feature","src/test/resources/Features/Product.feature","src/test/resources/Features/cart.feature"}
+@CucumberOptions(features={"src/test/resources/Features/Register.feature","src/test/resources/Features/Login2.feature",
+                           "src/test/resources/Features/Product.feature","src/test/resources/Features/cart.feature"},
                  glue="stepdef",
                  plugin="json:target/Cucumber reports.json")
 
